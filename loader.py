643f2099732c149ac6997c799e0edabc32074fb4
Ruslan Ziyazetdinov <ruslan.tiger@yandex.ru>
2022-04-19 22:47:22 +0300

    add functions:1) forming dict of all stocks; 2) downloading info about one stock for period

--- loader.py
+++ loader.py
@@ -2,30 +2,45 @@ import yfinance as yf
 import pandas as pd
 import requests
 import apimoex
-import asyncio
-import asyncio
-
-import aiohttp
-
-import aiomoex
-import pandas as pd
 
 
-async def main():
-    request_url = "https://iss.moex.com/iss/engines/stock/" "markets/shares/boards/TQBR/securities.json"
-    arguments = {"securities.columns": ("SECID," "REGNUMBER," "LOTSIZE," "SHORTNAME")}
+def form_dict_of_stocks():
+    request_url_imoex = "https://iss.moex.com/iss/statistics/engines/stock/markets/index/analytics/IMOEX/tickers.json"
+    request_url_imoex2 = "https://iss.moex.com/iss/statistics/engines/stock/markets/index/analytics/IMOEX2/tickers.json"
+    request_url_rtsi = "https://iss.moex.com/iss/statistics/engines/stock/markets/index/analytics/RTSI/tickers.json"
 
-    async with aiohttp.ClientSession() as session:
-        iss = aiomoex.ISSClient(session, request_url, arguments)
-        data = await iss.get()
-        df = pd.DataFrame(data["securities"])
-        df.set_index("SECID", inplace=True)
-        print(df.head(), "\n")
-        print(df.tail(), "\n")
-        df.info()
+    dict_stocks = {}
+    with requests.Session() as session:
+        iss = apimoex.ISSClient(session, request_url_imoex)
+        data_imoex = iss.get()
+        iss = apimoex.ISSClient(session, request_url_rtsi)
+        data_rtsi = iss.get()
+        iss = apimoex.ISSClient(session, request_url_imoex2)
+        data_imoex2 = iss.get()
+
+    for x in data_imoex['tickers']:
+        if x['ticker'] not in dict_stocks:
+            mas = [x['from'], x['till']]
+            dict_stocks[x['ticker']] = mas
+    for x in data_imoex2['tickers']:
+        if x['ticker'] not in dict_stocks:
+            mas = [x['from'], x['till']]
+            dict_stocks[x['ticker']] = mas
+    for x in data_rtsi['tickers']:
+        if x['ticker'] not in dict_stocks:
+            mas = [x['from'], x['till']]
+            dict_stocks[x['ticker']] = mas
+    return dict_stocks
 
 
-asyncio.run(main())
+def download_stock(name, from_date, to_date):
+    data = yf.download(name + '.ME', from_date, to_date)
+    return data
+
+
+
+
+
 
 
 
