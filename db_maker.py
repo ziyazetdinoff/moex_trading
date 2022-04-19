ab232b383200d3ce3047b972e798cfd9364934b2
Ruslan Ziyazetdinov <ruslan.tiger@yandex.ru>
2022-04-19 22:48:34 +0300

    created needed tables, add function of updating dict of all stocks

--- db_maker.py
+++ db_maker.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine, select, Table, Column, Integer, Float, Date, String, MetaData, ForeignKey
+import loader
 import os
 
 
@@ -6,28 +7,58 @@ meta = MetaData()
 user = os.environ['user']
 password = os.environ['password']
 
-stocks = Table('Stocks', meta,
-               Column('id_stock', Integer, primary_key=True),
-               Column('name', String(250), nullable=False))
+stocks = Table(
+    'Stocks', meta,
+    Column('id', Integer, primary_key=True),
+    Column('name', String(32)),
+    Column('from_date', Date),
+    Column('to_date', Date)
+)
 
-tradings = Table('Tradings', meta,
-                 Column('id_stock', Integer, ForeignKey("Stocks.id_stock")),
-                 Column('id_date', Integer),
-                 Column('date', Date),
-                 Column('Open', Float),
-                 Column('High', Float),
-                 Column('Low', Float),
-                 Column('Volume', Float))
+database = Table(
+    'Database', meta,
+    Column('id', Integer, primary_key=True),
+    Column('name', String(32)),
+    Column('from_dt', Date),
+    Column('till_dt', Date)
+)
 
+tradings = Table(
+    'Tradings', meta,
+    Column('id', Integer, primary_key=True),
+    Column('id_st', Integer, ForeignKey('Database.id')),
+    Column('date', Date),
+    Column('Open', Float),
+    Column('High', Float),
+    Column('Low', Float),
+    Column('Volume', Float)
+)
 
 # Подключаемся к БД и заносим данные
 # СУБД+драйвер://юзер:пароль@хост:порт/база
-engine = create_engine(f"mysql+mysqlconnector://{user}:{password}@localhost:3306/test_tradings", echo=True)
-meta.create_all(engine)
-ins = stocks.insert().values(name='APPL')
-conn = engine.connect()
-ins2 = tradings.insert().values(id_date='1', date='01-01-2015', Open=2.4, High=2.6, Low=2.2, Volume=200)
-conn.execute(ins)
-conn.execute(ins2)
+def connect_to_db():
+    engine = create_engine(f"mysql+mysqlconnector://{user}:{password}@localhost:3306/test_tradings", echo=True)
+    meta.create_all(engine)
+    return engine.connect()
+
+
+def update_list_of_stocks():
+    conn = connect_to_db()
+    delete = stocks.delete()
+    conn.execute(delete)
+    dict_stocks = loader.form_dict_of_stocks()
+    for x in dict_stocks:
+        ins_stock = stocks.insert().values(name=x, from_date=dict_stocks[x][0], to_date=dict_stocks[x][1])
+        conn.execute(ins_stock)
+
+
+def add_to_db(name, from_date, to_date):
+    pass
+
+
+
+
+
+
 
 
